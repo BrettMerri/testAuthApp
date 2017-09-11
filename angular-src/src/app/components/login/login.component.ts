8be2eac9e19e910ad9e278d83dafc2f7b29fcab4
Brett Merrifield <the.b.merri@gmail.com>
2017-09-11 09:16:33 -0400

    Added login and register pages to the angular front end

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if(data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        console.log(data.msg);
+        this.router.navigate(['dashboard']);
+      } else {
+        console.log(data.msg);
+        this.router.navigate(['login']);
+      }
+    });
   }
 
 }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +14,10 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(
+    private validateService: ValidateService,
+    private authService: AuthService,
+  private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,15 +29,27 @@ export class RegisterComponent implements OnInit {
       email: this.email,
       password: this.password
     }
-      // Required Fields
+    // Required Fields
     if(!this.validateService.validateRegister(user)) {
       console.log('Please fill in all fields');
       return false;
     }
+
+    // Validate Email
     if(!this.validateService.validateEmail(user.email)) {
       console.log('Please use a valid email');
       return false;
     }
 
+    // Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success) {
+        console.log('Success!');
+        this.router.navigate(['/login'])
+      } else {
+        console.log('Error!');
+        this.router.navigate(['/register']);
+      }
+    });
   }
 }
